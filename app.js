8eba433d3c91ba56d469b0d44955ed96947c5386
ouafabentaleb <ouafabentaleb@gmail.com>
2021-06-18 12:18:21 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Ouafa!'));
+app.get('/', (req, res) => res.send('The pipeline has been reviewed by Ouafa!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
